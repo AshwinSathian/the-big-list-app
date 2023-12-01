c1a345658ca7ba7b92c5f763e691e201b4c57208
Ashwin Sathian <ashwinsathyan19@gmail.com>
2023-12-02 05:06:40 +0530

    Added labels

--- src/app/components/tickets-list-item/tickets-list-item.component.ts
+++ src/app/components/tickets-list-item/tickets-list-item.component.ts
@@ -2,15 +2,22 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { TooltipModule } from 'primeng/tooltip';
+import { Priority } from '../../enums/priority.enum';
 import { Status } from '../../enums/status.enum';
 import { Ticket } from '../../interfaces/ticket';
 import { InitialsPipe } from '../../pipes/initials.pipe';
-import { Priority } from '../../enums/priority.enum';
+import { StringJoinPipe } from '../../pipes/string-join.pipe';
 
 @Component({
   selector: 'app-tickets-list-item',
   standalone: true,
-  imports: [CommonModule, CardModule, InitialsPipe, TooltipModule],
+  imports: [
+    CommonModule,
+    CardModule,
+    InitialsPipe,
+    TooltipModule,
+    StringJoinPipe,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './tickets-list-item.component.html',
   styleUrl: './tickets-list-item.component.css',
